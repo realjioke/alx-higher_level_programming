ff7217bce1b22dd3ee73fb73f1d550f27a3107ab
realjioke <scarlobest@gmail.com>
2023-02-28 13:14:36 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""add item module
-
-Deleted Attributes:
-    filename (str): Description
+"""
+ save items to a file.
 """
 
-import json
 import sys
 import os.path
 
-if __name__ == __main__:
+args = sys.argv[1:]
 
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    filename = "add_item.json"
-    if os.path.isfile(filename):
-        obj = load_from_json_file(filename)
-    else:
-        obj = []
-    obj.extend(sys.argv[1:])
-    save_to_json_file(obj, filename)
+item = []
+if os.path.exists("./add_item.json"):
+    item = load_from_json_file("add_item.json")
+
+save_to_json_file(item + args, "add_item.json")
